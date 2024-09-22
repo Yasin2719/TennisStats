7f8f7c9cbfddea21c599080d5847814e1f16b866
Yasin2719 <karakusyasin19@gmail.com>
2024-09-22 12:57:42 +0200

    feat(application): add player service

---
+++ src/Application/Services/IPlayerService.cs
@@ -0,0 +1,12 @@
+﻿using Application.DTOs;
+using Domain.Abstractions;
+using Domain.Models;
+
+namespace Application.Services;
+
+public interface IPlayerService
+{
+    Task<Result<IEnumerable<Player>>> GetPLayers();
+    Task<Result<Player>> GetPlayerById(int id);
+    Task<Result<StatsResponse>> GetStats();
+}
---
+++ src/Application/Services/PlayerService.cs
@@ -0,0 +1,41 @@
+﻿using Application.DTOs;
+using Application.Repositories;
+using Domain.Abstractions;
+using Domain.Errors;
+using Domain.Models;
+
+namespace Application.Services;
+
+public class PlayerService(
+    IPlayerRepository playerRepository, 
+    ICountryRepository countryRepository) : IPlayerService 
+{
+    private readonly IPlayerRepository _playerRepository = playerRepository;
+    private readonly ICountryRepository _countryRepository = countryRepository;
+
+    public async Task<Result<IEnumerable<Player>>> GetPLayers()
+    {
+        var players = await _playerRepository.GetPlayers();
+        return Result<IEnumerable<Player>>.Success(players);
+    }
+
+    public async Task<Result<Player>> GetPlayerById(int id)
+    {
+        var player = await _playerRepository.GetPlayerById(id);
+
+        if(player is null)
+        {
+            return Result<Player>.Failure(
+                PlayerError.NotFound, 
+                System.Net.HttpStatusCode.NotFound);
+        }
+
+        return Result<Player>.Success(player);
+    }
+
+    
+    public Task<Result<StatsResponse>> GetStats()
+    {
+        throw new NotImplementedException();
+    }
+}
