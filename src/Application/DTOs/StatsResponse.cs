868edf7369630412a7cb0f72bdeb5cf0e6e45e4e
Yasin2719 <karakusyasin19@gmail.com>
2024-09-22 12:21:01 +0200

    feat(application): add stats dto

---
+++ src/Application/DTOs/StatsResponse.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+
+namespace Application.DTOs;
+
+public class StatsResponse(Country country, decimal imcMoyen, int tailleMediane)
+{
+    public Country Country { get; set; } = country;
+    public decimal IMCMoyen { get; set; } = imcMoyen;
+    public int TailleMediane { get; set; } = tailleMediane;
+}
