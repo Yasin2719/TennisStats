d08eac9f69a275a975a3c69f9e4dcf70e7dc4121
Yasin2719 <karakusyasin19@gmail.com>
2024-09-22 11:58:37 +0200

    feat(domain): add result & error class

---
+++ src/Domain/Abstractions/Result.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+
+namespace Domain.Abstractions;
+
+public class Result<T> where T : class?
+{
+    private Result(Error error, HttpStatusCode statusCode)
+    {
+        IsSucess = false;
+        StatusCode = statusCode;
+        Errors = [error];
+    }
+
+    private Result(T? data = null, HttpStatusCode statusCode = HttpStatusCode.OK)
+    {
+
+        IsSucess = true;
+        StatusCode = statusCode;
+        Errors = [Error.None];
+        Data = data;
+    }
+
+    public bool IsSucess { get; }
+
+    public bool IsFailure => !IsSucess;
+
+    public List<Error> Errors { get; }
+
+    public T? Data { get; }
+
+    public HttpStatusCode StatusCode { get; }
+
+    public static Result<T> Success(T data, HttpStatusCode code = HttpStatusCode.OK) => new(data, code);
+
+    public static Result<T> Failure(Error error, HttpStatusCode code = HttpStatusCode.BadRequest) => new(error, code);
+}
