049d3d022f7bd10ced7afdbe2eef54c4faf7935c
Yasin2719 <karakusyasin19@gmail.com>
2024-09-22 12:06:28 +0200

    feat(domain): add players errors

---
+++ src/Domain/Errors/PlayerError.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Abstractions;
+
+namespace Domain.Errors;
+
+public sealed class PlayerError
+{
+    public static readonly Error NotFound = new("Player.NotFound", "The player was not found");
+}
