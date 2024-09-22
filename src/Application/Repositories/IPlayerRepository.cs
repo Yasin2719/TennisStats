6b6f6f1bc1735d096a931515d062a8995a6b332c
Yasin2719 <karakusyasin19@gmail.com>
2024-09-22 12:16:49 +0200

    feat(application): add repository interfaces

---
+++ src/Application/Repositories/IPlayerRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Application.Repositories;
+
+public interface IPlayerRepository
+{
+    Task<IEnumerable<Player>> GetPlayers();
+    Task<Player> GetPlayerById(int id);
+    Task<int> GetIMCMoyen();
+    Task<int> GetTailleMediane();
+}
